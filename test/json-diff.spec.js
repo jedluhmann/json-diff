cdc216b6e8f1d01e27c7d6f6fd7984f981631142
jedluhmann <jedluhmann@gmail.com>
2026-09-05 22:48:24 -0500

    chore: ran lint:fix

--- test/json-diff.spec.js
+++ test/json-diff.spec.js
@@ -250,17 +250,36 @@ describe('diff', () => {
     it("should return [[' ', <unchanged item>], ['~', <diff>], [' ', <unchanged item>]] for two arrays when an item has been modified", () => {
       assert.deepEqual(
         diff(
-          [{"holes": [{ "depth": 11.5, "diameter": 8, "start": { "x": 218, "y": 241, "z": 9 }, "end": { "x": 220, "y": 241, "z": -2.5 }, "plankFaceId": "0", "name": "短木销_修改", "type": "PLANK" }, { "depth": 11.5, "diameter": 8, "start": { "x": 218, "y": 241, "z": 9 }, "end": { "x": 220, "y": 241, "z": -2.5 }, "plankFaceId": "0", "name": "短木销_修改", "type": "PLANK" }]}],
-          [{"holes": [{ "depth": 11.5, "diameter": 8, "start": { "x": 218, "y": 241, "z": 9 }, "end": { "x": 218, "y": 241, "z": -2.5 }, "plankFaceId": "0", "name": "短木销", "type": "PLANK" }, { "depth": 11.5, "diameter": 8, "start": { "x": 218, "y": 241, "z": 9 }, "end": { "x": 218, "y": 241, "z": -2.5 }, "plankFaceId": "0", "name": "短木销", "type": "PLANK" }]}],
+          [
+            {
+              holes: [
+                { depth: 11.5, diameter: 8, start: { x: 218, y: 241, z: 9 }, end: { x: 220, y: 241, z: -2.5 }, plankFaceId: '0', name: '短木销_修改', type: 'PLANK' },
+                { depth: 11.5, diameter: 8, start: { x: 218, y: 241, z: 9 }, end: { x: 220, y: 241, z: -2.5 }, plankFaceId: '0', name: '短木销_修改', type: 'PLANK' },
+              ],
+            },
+          ],
+          [
+            {
+              holes: [
+                { depth: 11.5, diameter: 8, start: { x: 218, y: 241, z: 9 }, end: { x: 218, y: 241, z: -2.5 }, plankFaceId: '0', name: '短木销', type: 'PLANK' },
+                { depth: 11.5, diameter: 8, start: { x: 218, y: 241, z: 9 }, end: { x: 218, y: 241, z: -2.5 }, plankFaceId: '0', name: '短木销', type: 'PLANK' },
+              ],
+            },
+          ],
           {}
         ),
-        [["~", {
-          "holes": [
-            ["~", {"end": {"x": {"__old": 220, "__new": 218}}, "name": {"__old": "短木销_修改", "__new": "短木销"}}],
-            ["~", {"end": {"x": {"__old": 220, "__new": 218}}, "name": {"__old": "短木销_修改", "__new": "短木销"}}]
-          ]
-        }]]
-      )
+        [
+          [
+            '~',
+            {
+              holes: [
+                ['~', { end: { x: { __old: 220, __new: 218 } }, name: { __old: '短木销_修改', __new: '短木销' } }],
+                ['~', { end: { x: { __old: 220, __new: 218 } }, name: { __old: '短木销_修改', __new: '短木销' } }],
+              ],
+            },
+          ],
+        ]
+      );
     });
   });
 
